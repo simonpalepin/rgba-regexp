a1a3724ce8e529f87db7ff94b17bd92925629b99
Simon-Pierre Alepin <s.alepin@hotmail.com>
2021-04-23 21:00:54 -0400

    Adds missing tests for percentRGBACss4

--- src/percentRGBACss4.spec.ts
+++ src/percentRGBACss4.spec.ts
@@ -4,10 +4,12 @@ describe('percentRGBACss4 regular expression with capture groups', () => {
   it('should match css4 rgba(<percent> <percent> <percent> / <number>)', () => {
     expect(percentRGBACss4.test('rgba(100% 10.55% 0% / 1)')).toEqual(true);
     expect(percentRGBACss4.test('rgba(0.100% .55% 0%  /.22)')).toEqual(true);
+    expect(percentRGBACss4.test('rgba(0.100% .55% 0%  .22)')).toEqual(false);
   });
   
   it('should match css4 rgba(<percent> <percent> <percent> / <percent>)', () => {
     expect(percentRGBACss4.test('rgba(100% 10.55% 0% / 1%)')).toEqual(true);
     expect(percentRGBACss4.test('rgba(0.100% .55% 0%  /.22%)')).toEqual(true);
+    expect(percentRGBACss4.test('rgba(0.100% .55% 0% .22%)')).toEqual(false);
   });
 });
